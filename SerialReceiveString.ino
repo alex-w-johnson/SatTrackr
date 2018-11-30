4d81cf7b8c5a3289208a1c1799938ef098f232b8
alex-w-johnson <44879877+alex-w-johnson@users.noreply.github.com>
2018-11-29 16:07:06 -0800

    Add files via upload

---
+++ SerialReceiveString.ino
@@ -0,0 +1,26 @@
+
+String receivedString = "";
+
+void setup() {
+Serial.begin(9600);
+Serial.print("started\n");
+}
+
+void loop() {
+  while(Serial.available()>0){
+    int CharReceived = Serial.read();
+    //if(isDigit(CharReceived)){
+    if(1){
+      receivedString += (char)CharReceived;
+    }
+    if(CharReceived=='|'){
+      Serial.print("received: ");
+      Serial.println(receivedString);
+      int receivedValue = receivedString.toInt();
+      Serial.print("value: ");
+      Serial.println(receivedValue);
+      receivedString = "";
+    }
+  }
+}
+
