f96105de073023a3017aa528c23cf3884f08f04c
alex-w-johnson <44879877+alex-w-johnson@users.noreply.github.com>
2018-11-29 16:39:18 -0800

    Update SerialReceiveString.ino

--- SerialReceiveString.ino
+++ SerialReceiveString.ino
@@ -1,6 +1,8 @@
 
 String receivedString = "";
-
+static int receivedValue = 0;
+static int receivedArray[6] = [];
+static int checksum = 0;
 void setup() {
 Serial.begin(9600);
 Serial.print("started\n");
@@ -13,13 +15,46 @@ void loop() {
     if(1){
       receivedString += (char)CharReceived;
     }
-    if(CharReceived=='|'){
-      Serial.print("received: ");
-      Serial.println(receivedString);
-      int receivedValue = receivedString.toInt();
-      Serial.print("value: ");
-      Serial.println(receivedValue);
-      receivedString = "";
+    int idx = 0;
+    while(idx < 6;){
+      if(CharReceived=='|'){
+        Serial.println("Starting to read row.");
+        Serial.print("received: ");
+        Serial.println(receivedString);
+        receivedValue = receivedString.toInt();
+        Serial.print("value: ");
+        Serial.println(receivedValue);
+        receivedString = "";
+        receivedArray[idx] = receivedValue;
+        checksum += receivedValue;
+        idx++;
+      }
+      else if(CharReceived==','){
+        Serial.print("received: ");
+        Serial.println(receivedString);
+        receivedValue = receivedString.toInt();
+        Serial.print("value: ");
+        Serial.println(receivedValue);
+        receivedString = "";
+        receivedArray[idx] = receivedValue;
+        checksum += receivedValue;
+      else if(CharReceived==';'){
+        if(checksum != receivedValue){
+          Serial.println("PANIC NOW CHKSUM IS DIFFERENT YA DINGUS!")
+        }
+        Serial.print("Row recieved:    ");
+        for(int printidx = 0; printidx <= idx; printidx++){
+          Serial.print(recievedArray[printidx]);
+          Serial.print("    ");
+        }
+        Serial.println("Proceeding to next row!");
+      else{
+        Serial.println("SOMETHING ELSE WENT WRONG YA DINGUS!");
+      }
+        
+          
+      }
+        
     }
   }
 }
